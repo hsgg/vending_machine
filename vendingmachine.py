64e9f263c7ed9ed486b5d881ccca5a04153b1fe6
efong <erin.fong@tufts.edu>
2014-06-24 09:44:15 -0400

    Initial code for vending machine

---
+++ vendingmachine.py
@@ -0,0 +1,14 @@
+vm = VendingMachine()
+
+class water(object):
+	def __init__(self):
+		self.stock = 5
+		self.price = 1.50
+
+class 		
+
+vm.restock("water", quantity=5, price=1.50)
+vm.restock("iced tea", quantity=10, price=2.50)
+vm.restock("soda", quantity=12, price=2.00)
+
+
