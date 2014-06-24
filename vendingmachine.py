12029b1d08343b77563a4efaae51425b3384584e
efong <erin.fong@tufts.edu>
2014-06-24 09:50:02 -0400

    2nd commit

--- vendingmachine.py
+++ vendingmachine.py
@@ -1,11 +1,9 @@
 vm = VendingMachine()
 
-class water(object):
+class drink(object):
 	def __init__(self):
 		self.stock = 5
 		self.price = 1.50
-
-class 		
 
 vm.restock("water", quantity=5, price=1.50)
 vm.restock("iced tea", quantity=10, price=2.50)
